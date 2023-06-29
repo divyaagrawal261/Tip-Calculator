c49ab3a8ae452987248522c0265b7deee33cd3bb
Bunny <divyaagrawal0747@gmail.com>
2023-06-29 17:30:05 +0530

    app.js completed

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ let Numb=Number(numPeople.innerText)
 
 const calculateBill = () => 
 {
-    let bill=(billInput.value+ ((tip.value/100)*billInput.value))/Numb
-    amountPerHead.innerText=bill
+    let bill=Number(billInput.value) + (Number(tip.value)/100*Number(billInput.value))
+    amountPerHead.innerText=bill/Numb
 }
 
 const increment=()=>{
     Numb=Numb+1
+    numPeople.innerText=Numb    
+    calculateBill()
+}
+
+const decrement=()=>{
+    if(Numb>1)
+       Numb=Numb-1
     numPeople.innerText=Numb    
     calculateBill()
 }
