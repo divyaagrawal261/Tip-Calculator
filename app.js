ff315fa0db13b596d827a333d500d0da0fae489e
Bunny <divyaagrawal0747@gmail.com>
2023-06-29 18:38:26 +0530

    Added Style.css

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-const billInput=document.getElementById("billAmount")
-const tip=document.getElementById("tipPercentage")
-const numPeople=document.getElementById("people")
-const amountPerHead=document.getElementById("amount")
+const billInput = document.getElementById("billAmount");
+const tip = document.getElementById("tipPercentage");
+const numPeople = document.getElementById("people");
+const amountPerHead = document.getElementById("amount");
 
-let Numb=Number(numPeople.innerText)
+let Numb = Number(numPeople.innerText);
 
-const calculateBill = () => 
-{
-    let bill=Number(billInput.value) + (Number(tip.value)/100*Number(billInput.value))
-    amountPerHead.innerText=bill/Numb
-}
+const calculateBill = () => {
+  let bill =Number(billInput.value)+ (Number(tip.value) / 100) * Number(billInput.value);
+  amountPerHead.innerText = (bill / Numb).toFixed(2);
+};
 
-const increment=()=>{
-    Numb=Numb+1
-    numPeople.innerText=Numb    
-    calculateBill()
-}
+const increment = () => {
+  Numb = Numb + 1;
+  numPeople.innerText = Numb;
+  calculateBill();
+};
 
-const decrement=()=>{
-    if(Numb>1)
-       Numb=Numb-1
-    numPeople.innerText=Numb    
-    calculateBill()
-}
+const decrement = () => {
+  if (Numb > 1) Numb = Numb - 1;
+  numPeople.innerText = Numb;
+  calculateBill();
+};
